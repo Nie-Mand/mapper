027e43ac923c690bf2f48fea19df3a6b72264256
Nie-Mand <chill.out.x19@gmail.com>
2023-03-19 02:47:53 +0100

    fixed

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
 import ReactLeafletGoogleLayer from "react-leaflet-google-layer";
+import "leaflet/dist/leaflet.css";
 
 const data = [
   {
     id: 1,
     metadata: "hey",
-    position: [36.3759586, 10.5428839],
+    position: [36.391783, 10.55166],
   },
 ];
 
@@ -28,10 +29,15 @@ function App() {
 
   return (
     <div>
-      <MapContainer center={position} zoom={13} scrollWheelZoom={false}>
-        <ReactLeafletGoogleLayer
-          apiKey="AIzaSyDi4EE5gzhlkVgdkTefE-irT7AivylpaGo"
-          type={"satellite"}
+      <MapContainer
+        center={position}
+        zoom={13}
+        scrollWheelZoom={false}
+        height="500"
+      >
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
 
         {data.map((v) => (
